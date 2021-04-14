722d848562eea6fe2cd81b0bdb424865c7d49a44
chrislemus <icrislemus@gmail.com>
2021-04-14 19:45:59 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(({ name, movies }) => {
+        return (
+          <div key={name}>
+            <h6>{name}</h6>
+            <ul>
+              {movies.map((movie) => (
+                <li key={movie}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(({ title, time, genres, metascore }, idx) => {
+        return (
+          <div key={"movies-" + idx}>
+            <h6>{title}</h6>
+            <p>{time}</p>
+            <ul>
+              {genres.map((g, idx) => (
+                <li key={title + g}>{g}</li>
+              ))}
+            </ul>
+            {metascore && <p>{metascore}</p>}
+          </div>
+        );
+      })}
     </div>
   );
 };
